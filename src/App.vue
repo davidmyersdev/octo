e2f5eea598cb989f9393306fa2b1aa70ad212504
David R. Myers <davidrmyersii@gmail.com>
2020-08-28 13:00:26 -0400

    Fix context close button

--- src/App.vue
+++ src/App.vue
@@ -151,7 +151,7 @@
         </span>
         <span v-else  class="context-placeholder">no active tags</span>
         <div @click="deactivateContext" class="context-close">
-          <svg class="bi bi-x-square-fill" width="1.25em" height="1.25em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <svg class="close-context bi bi-x-square-fill" width="1.25em" height="1.25em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm9.854 4.854a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z"/>
           </svg>
         </div>
@@ -594,7 +594,7 @@ pre {
   display: none !important;
 }
 
-svg {
+svg:not(.close-context) {
   flex-shrink: 0;
   margin-right: 0.5em;
 }

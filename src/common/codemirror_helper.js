b13b446403829f930779cfbcff596027a88259d3
David R. Myers <davidrmyersii@gmail.com>
2020-09-01 23:53:40 -0400

    Migrate to vue-codemirror

---
+++ src/common/codemirror_helper.js
@@ -0,0 +1,61 @@
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/meta';
+
+let modes = [];
+
+const addModeScript = (mode, options) => {
+  const script = document.createElement('script');
+
+  // pull mode in via CDN
+  script.src = getModeUrl(mode);
+
+  if (options.onload) {
+    script.onload = options.onload;
+  }
+
+  if (options.onerror) {
+    script.onerror = options.onerror;
+  }
+
+  document.body.appendChild(script);
+};
+
+const getModeUrl = (mode) => {
+  // hardcode for now
+  return `https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.54.0/mode/${mode.mode}/${mode.mode}.min.js`;
+};
+
+const fuzzyFindMode = (fuzzy) => {
+  const lowerFuzzy = fuzzy.toLowerCase();
+
+  return CodeMirror.modeInfo.find((mode) => {
+    // check to see if the mode matches
+    if (mode.mode === lowerFuzzy) {
+      return true;
+    }
+
+    // check to see if the extension matches
+    if (mode.ext && mode.ext.includes(lowerFuzzy)) {
+      return true;
+    }
+
+    return false;
+  });
+};
+
+export const loadMode = (fuzzyMode, options) => {
+  const mode = fuzzyFindMode(fuzzyMode);
+
+  if (mode && mode.mode !== 'null' && !modes.some(m => m.mode === mode.mode)) {
+    modes.push(mode);
+
+    // ensure CodeMirror namespace exists in the browser
+    window.CodeMirror = window.CodeMirror || CodeMirror;
+
+    addModeScript(mode, options);
+  }
+};
+
+export default {
+  loadMode,
+};

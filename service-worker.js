d2a6a5027440a0d944c6065b314b1ae3b221fe13
David R. Myers <davidrmyersii@gmail.com>
2020-09-03 21:56:58 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ffa7dfcbacc8c038ad8e83a1a93544c.js"
+  "/precache-manifest.74e508c58627c59bbb4fdb95515cac8a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "octo"});

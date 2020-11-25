6ffba437fe7aa6f44096ed79e03d55efcb691aef
David R. Myers <davidrmyersii@gmail.com>
2020-11-25 13:16:26 -0500

    Persist keyMap setting

--- src/store/plugins/caching/settings.js
+++ src/store/plugins/caching/settings.js
@@ -4,6 +4,7 @@ import {
   LOAD_SETTINGS,
   SET_CRYPTO_ENABLED,
   SET_CRYPTO_KEYS,
+  SET_EDITOR_KEY_MAP,
   SET_EDITOR_TAB_SIZE,
   SETTINGS_LOADED,
 } from '@/store/modules/settings';
@@ -28,6 +29,7 @@ export default (store) => {
     switch (type) {
       case SET_CRYPTO_ENABLED:
       case SET_CRYPTO_KEYS:
+      case SET_EDITOR_KEY_MAP:
       case SET_EDITOR_TAB_SIZE:
         cache.setItem(CACHE_KEY, state.settings);
 

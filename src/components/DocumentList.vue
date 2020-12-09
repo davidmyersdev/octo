df783b7da28fedc8efbb1006ec4bf1aa4440ca51
David R. Myers <davidrmyersii@gmail.com>
2020-12-08 22:50:45 -0500

    Limit the amount of visible documents in the list view (for performance)

--- src/components/DocumentList.vue
+++ src/components/DocumentList.vue
@@ -37,7 +37,17 @@
     <div>
       <p v-if="isEditing" class="text-muted">Select two or more documents to merge them together.</p>
     </div>
-    <Document v-for="document in finalDocuments" @click.native="selectDocument(document.id)" :key="document.id" v-bind="document"></Document>
+    <Document v-for="document in visibleDocuments" @click.native="selectDocument(document.id)" :key="document.id" v-bind="document"></Document>
+    <div class="d-flex justify-content-center mb-5">
+      <div v-if="showLoadMore" @click="loadMore" class="btn btn-secondary p-3 d-flex align-items-center">
+        <svg width="1.25em" height="1.25em" viewBox="0 0 16 16" class="bi bi-file-earmark-arrow-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
+          <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
+          <path fill-rule="evenodd" d="M8 6a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 10.293V6.5A.5.5 0 0 1 8 6z"/>
+        </svg>
+        <span class="ml-3">Load More</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -69,6 +79,7 @@ export default {
       filterText: '',
       isEditing: false,
       selectedDocuments: [],
+      visibleCount: 25,
     };
   },
   computed: {
@@ -135,10 +146,19 @@ export default {
         selected: this.selectedDocuments.includes(doc),
       }));
     },
+    showLoadMore() {
+      return this.visibleCount <= this.finalDocuments.length;
+    },
+    visibleDocuments() {
+      return this.finalDocuments.slice(0, this.visibleCount);
+    },
   },
   methods: {
     focus() {
       this.$refs.input.focus();
+    },
+    loadMore() {
+      this.visibleCount += 25;
     },
     mergeDocuments() {
       this.$store.dispatch(MERGE_DOCUMENTS, this.selectedDocuments);

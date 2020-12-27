0e4420a771e488521963142aee7b8b7c1df7f612
David R. Myers <davidrmyersii@gmail.com>
2020-12-27 14:35:01 -0500

    Clean up the Exporter

--- src/components/Exporter.vue
+++ src/components/Exporter.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container-lg d-flex">
-    <div class="editor w-100">
-      <MarkdownEditor ref="editable" class="editable" :value="value" />
-    </div>
+    <MarkdownEditor ref="editable" class="editable w-100" :value="value" />
   </div>
 </template>
 
@@ -29,21 +27,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-  /* Duped from TheEditor for now */
-  .editor {
-    background: url('~@/assets/octopus-transparent.svg') center center no-repeat;
-    background-size: 50% 50%;
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    margin: auto;
-    min-height: 30rem;
-  }
-
-  .editor .editable {
-    outline: none;
-    white-space: pre-wrap;
-  }
-</style>

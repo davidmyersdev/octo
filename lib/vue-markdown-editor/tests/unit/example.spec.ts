30980e884fae67d3d877982a6c77856860adef59
David R. Myers <davidrmyersii@gmail.com>
2021-01-02 17:50:35 -0500

    Move vue-markdown-editor to its own repo

--- lib/vue-markdown-editor/tests/unit/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import App from '@/components/MarkdownEditor.vue'
-
-test('App should work', () => {
-  const wrapper = shallowMount(App)
-  expect(wrapper.text()).toMatch('')
-})

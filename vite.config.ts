7a6df6dfe82a4a706b38ffc69f7cfd9b636dac57
David R. Myers <davidrmyersii@gmail.com>
2021-09-30 23:05:56 -0400

    Auto-update the service worker

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     createVuePlugin(),
     injectHtml({ data: parsed }),
     VitePWA({
+      registerType: 'autoUpdate',
       strategies: 'generateSW',
       workbox: {
         additionalManifestEntries: [

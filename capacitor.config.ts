f65eb4c35398dd63a522b6ee379bc86f03f4d248
David R. Myers <davidrmyersii@gmail.com>
2022-02-02 09:44:30 -0500

    Init capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.octo',
+  appName: 'Octo',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

1862f4ac298430dc45aac953c6202c1da2064111
voraciousdev <voraciousdev@users.noreply.github.com>
2022-04-01 03:08:53 +0000

    deploy: 63809465faadc9ab780b57737ca83202d0403753

--- assets/TagLink.df85789a.js
+++
@@ -1,1 +0,0 @@
-import{T as s}from"./Tag.ce93ab96.js";import{r as n,a as c,d as l,w as e,b as i,i as m,t as p}from"./index.6afa439f.js";const f={props:["tag"],setup(t){const{tag:a}=t,o={name:"tagged_docs",params:{tag:a}};return(u,_)=>{const r=n("router-link");return c(),l(r,{to:o,class:"flex items-center"},{default:e(()=>[i(s,null,{default:e(()=>[m(p(t.tag),1)]),_:1})]),_:1})}}};export{f as _};

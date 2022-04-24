412b58ae933dd94a3343604e26fb9922fe224b6c
voraciousdev <voraciousdev@users.noreply.github.com>
2022-04-24 17:25:22 +0000

    deploy: 4c3e259548a12116302279ad8a78c1bd4cc57eac

---
+++ assets/Daily.0df8e363.js
@@ -0,0 +1,12 @@
+import{_ as i,ab as n,l as a,X as d,a1 as c,ac as l,r as u,a as m,d as h,f as _}from"./index.ffc7d12a.js";import f from"./Editor.107cf022.js";import"./Editor.4af8d90d.js";const y={name:"Daily",components:{Editor:f},computed:{daily(){return this.$store.getters.daily}},methods:{async buildTemplate(){try{const t=(await n(()=>import("./quotes.6145aced.js"),[])).default,e=t[Math.floor(Math.random()*t.length)];return`#daily
+
+# ${a().format("dddd, MMMM Do, YYYY")}
+
+> ${e.text}
+> ${e.author||"Unknown"}
+
+`}catch(t){return console.error(t),`#daily
+
+# ${a().format("dddd, MMMM Do, YYYY")}
+
+`}},async checkDaily(){let t=a().startOf("day").add(3,"hours");if(a()<t&&(t=t.subtract(1,"day")),!this.daily||this.daily.createdAt<t){const e=await this.buildTemplate(),o=new d({text:e,daily:!0});this.$store.commit(c,o)}}},beforeMount(){this.$store.state.documents.loaded?this.checkDaily():this.$store.subscribe(({type:t},e)=>{t===l&&this.checkDaily()})}};function p(t,e,o,D,M,r){const s=u("Editor");return r.daily?(m(),h(s,{key:0,id:r.daily.id,initialFocus:"end"},null,8,["id"])):_("",!0)}var Y=i(y,[["render",p]]);export{Y as default};

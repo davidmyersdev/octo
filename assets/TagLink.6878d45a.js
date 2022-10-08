4e41dd0d10d0c0b6e1a87add84d6552f4f2dd131
voracious <voracious@users.noreply.github.com>
2022-10-08 21:29:50 +0000

    deploy: f0a0a329e220dfe0d933d3d073848ab00a9ec366

---
+++ assets/TagLink.6878d45a.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./Tag.61a017bb.js";import{r,o as c,n as l,w as e,b as i,f as _,u as m}from"./index.b6a09ca3.js";const g={name:"TagLink",props:["tag"],setup(t){const{tag:o}=t,a={path:`/docs/t/${o}`};return(u,p)=>{const s=r("router-link");return c(),l(s,{to:a,class:"flex items-center"},{default:e(()=>[i(n,null,{default:e(()=>[_(m(t.tag),1)]),_:1})]),_:1})}}};export{g as _};

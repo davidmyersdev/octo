3039db6fd56b12f27d35fa1b2f618a96bc7e1bf2
lyqht <estee.tsw@gmail.com>
2022-10-14 00:34:18 +0800

    added file input and remarkable for importing & parsing markdown files

--- pages/docs/import.vue
+++ pages/docs/import.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container mx-auto p-4 flex flex-col flex-grow h-full gap-4">
-    <div>
-      <button @click="importDocs" class="button button-size-medium button-color-gray">Import Docs</button>
+    <div class="flex flex-col gap-4">
+      <label for="markdown-file-uploader">Choose .md file(s) to upload</label>
+      <input ref="uploads" @change="parseFiles" type="file" id="markdown-file-uploader" multiple accept=".md" class="input" />
     </div>
-    <pre @input="updateText" contenteditable ref="editable" class="editable monospace min-h- w-full text-current bg-transparent outline-none flex-grow"></pre>
   </div>
 </template>
 
 <script>
 import Doc from '/src/models/doc.js'
+import { Remarkable } from 'remarkable';
+import frontMatterPlugin from 'remarkable-front-matter';
+var md = new Remarkable();
+md.use(frontMatterPlugin);
 
 export default {
   data() {
@@ -17,28 +21,43 @@ export default {
     }
   },
   methods: {
-    importDocs() {
-      const docs = [JSON.parse(this.text)].flat()
-      
+    parseFiles(event) {
+      const files = [...(this.uploads().files ? this.uploads().files : [])];
+      files.forEach(file => {
+        const reader = new FileReader()
+        reader.onerror = error => reject(error)
+        reader.onload = event => {
+          const fileText = event.target.result
+          const env = { frontMatter: undefined };
+          md.render(fileText, env)
+          const { id } = env.frontMatter;
 
-      docs.forEach(({ id, text }) => {
-        if (id) {
-          const existingDoc = this.$store.state.documents.all.find(doc => doc.id === id)
+          const text = fileText.replace(/---(.|\n)*---/, "")
+          this.importDoc({ id, text });
+        }
 
-          if (existingDoc) {
-            return this.$store.commit('EDIT_DOCUMENT', {
-              id: existingDoc.id,
-              text,
-            })
-          } else {
-            this.$store.commit('ADD_DOCUMENT', new Doc({ text }))
-          }
+        reader.readAsText(file)
+      })
+
+    },
+    uploads() {
+      return this.$refs.uploads;
+    },
+    importDoc({ id, text }) {
+      if (id) {
+        const existingDoc = this.$store.state.documents.all.find(doc => doc.id === id)
+
+        if (existingDoc) {
+          return this.$store.commit('EDIT_DOCUMENT', {
+            id: existingDoc.id,
+            text,
+          })
         } else {
           this.$store.commit('ADD_DOCUMENT', new Doc({ text }))
         }
-      })
-
-      this.$refs.editable.innerText = ''
+      } else {
+        this.$store.commit('ADD_DOCUMENT', new Doc({ text }))
+      }
     },
     updateText(event) {
       this.text = event.target.innerText

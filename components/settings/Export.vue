17a07e9cca99f0dee486d746046d98971ebbfbf3
David R. Myers <davidrmyersii@gmail.com>
2022-10-17 10:58:03 -0400

    Update terminology

--- components/settings/Export.vue
+++ components/settings/Export.vue
@@ -7,7 +7,7 @@ import Exporter from '/pages/docs/export.vue'
     <h3 class="text-3xl">Export Docs</h3>
     <hr class="mt-2 mb-4">
     <div class="mb-4">
-      <label>This tool lets you download your knowledge base as a zipped folder of plain-text Markdown files.</label>
+      <label>This tool lets you export your knowledge base as a zipped folder of plain-text Markdown files.</label>
     </div>
     <div class="mb-4">
       <Exporter />

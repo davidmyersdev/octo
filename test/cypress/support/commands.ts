d1499cf16a6552a3d665d30e662bf623b3d8d2bb
David R. Myers <davidrmyersii@gmail.com>
2022-12-21 10:44:12 -0500

    Make sure auth is also cleared with IDB

--- test/cypress/support/commands.ts
+++ test/cypress/support/commands.ts
@@ -1,8 +1,29 @@
 import localforage from 'localforage'
 
+const signOut = async () => {
+  const firebaseDb = localforage.createInstance({
+    name: 'firebaseLocalStorageDb',
+    storeName: 'firebaseLocalStorage',
+  })
+
+  const keys = await firebaseDb.keys()
+
+  return Promise.all(
+    keys.map((key) => {
+      firebaseDb.removeItem(key)
+    })
+  )
+}
+
 // https://github.com/cypress-io/cypress/issues/1208
 Cypress.Commands.add('clearIDB', () => {
   const names = ['contexts', 'firebase/documents', 'settings']
 
-  return Promise.all(names.map(name => localforage.createInstance({ name }).clear()))
+  return new Cypress.Promise((resolve, reject) => {
+    signOut().then(() => {
+      return Promise.all(names.map(name => localforage.createInstance({ name }).clear())).then(things => {
+        resolve(things)
+      })
+    }).catch(reject)
+  })
 })

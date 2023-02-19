373817cc83034c0e93a13796dc78a71d7a4d075f
David R. Myers <davidrmyersii@gmail.com>
2023-02-19 16:23:33 -0500

    Integrate codemirror-lang-mermaid

--- src/vendor/plugins/mermaid/index.ts
+++ src/vendor/plugins/mermaid/index.ts
@@ -1,8 +1,12 @@
+import { language } from './language'
+import { theme } from './theme'
 import { widget } from './widget'
 import type { Config } from '../index'
 
 export const mermaid = async (config: Config) => {
   return [
+    { type: 'default', value: theme() },
     { type: 'default', value: await widget(config) },
+    { type: 'language', value: language() },
   ]
 }

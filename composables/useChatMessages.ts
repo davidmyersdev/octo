45428d18b9b3b79789fc87777bfbf5bd78165136
David R. Myers <davidrmyersii@gmail.com>
2023-06-18 16:24:49 -0400

    Add composables for chats and chat histories

---
+++ composables/useChatMessages.ts
@@ -0,0 +1,21 @@
+import { type ChatMessage } from '#root/helpers/database'
+
+export const useChatMessage = (id?: Ref<string>) => {
+  const { db } = useDatabase()
+  const { result: chatMessage } = useQuery(() => db.chatMessages.get(id?.value ?? ''))
+
+  return {
+    addChatMessage: (chatMessage: ChatMessage) => db.chatMessages.add(chatMessage),
+    chatMessage,
+  }
+}
+
+export const useChatMessages = ({ chatId }: { chatId: Ref<string> }) => {
+  const { db } = useDatabase()
+  const { result: chatMessages } = useQuery(() => db.chatMessages.where({ chatId: chatId.value }).sortBy('createdAt'), [])
+
+  return {
+    addChatMessage: (chatMessage: ChatMessage) => db.chatMessages.add(chatMessage),
+    chatMessages,
+  }
+}

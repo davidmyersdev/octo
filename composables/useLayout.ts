be72b76801d85322a6385a102b39a2323192daf3
David R. Myers <davidrmyersii@gmail.com>
2023-07-08 17:42:48 -0400

    Make layout hydration safe

--- composables/useLayout.ts
+++ composables/useLayout.ts
@@ -1,19 +1,12 @@
 import { useStorage } from '@vueuse/core'
 
 export const useLayout = () => {
-  const showMenu = ref(false)
-  const showMeta = ref(false)
+  const { lazyComputed } = useHooks()
 
   const showMenuCache = useStorage<boolean>('showMenu', false)
-  const showMetaCache = useStorage<boolean>('showMeta', true)
-
-  onMounted(() => {
-    showMenu.value = showMenuCache.value
-    showMeta.value = showMetaCache.value
-  })
-
-  watch(showMenu, () => showMenuCache.value = showMenu.value)
-  watch(showMeta, () => showMetaCache.value = showMeta.value)
+  const showMetaCache = useStorage<boolean>('showMeta', false)
+  const showMenu = lazyComputed(() => showMenuCache.value)
+  const showMeta = lazyComputed(() => showMetaCache.value)
 
   const toggleMenu = () => showMenu.value = !showMenu.value
   const toggleMeta = () => showMeta.value = !showMeta.value

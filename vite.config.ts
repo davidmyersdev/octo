fb89389953214daffd674a199b210ca0583b6645
David R. Myers <davidrmyersii@gmail.com>
2023-10-15 13:13:00 -0400

    Configure Nuxt dev server to work with Tauri

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,26 @@ export const config = {
   test: {
     environment: 'happy-dom',
   },
+  // For Tauri
+  clearScreen: false,
+  // Enable environment variables
+  // Additional environment variables can be found at
+  // https://tauri.app/2/reference/environment-variables/
+  envPrefix: ['VITE_', 'TAURI_'],
+  server: {
+    // Tauri requires a consistent port
+    strictPort: true,
+    // Enables the development server to be discoverable by other devices for mobile development
+    host: '0.0.0.0',
+    hmr: {
+      // Use websocket for mobile hot reloading
+      protocol: 'ws',
+      // Make sure it's available on the network
+      host: '0.0.0.0',
+      // Use a specific port for hmr
+      port: 5183,
+    },
+  },
 } satisfies UserConfig
 
 // https://vitejs.dev/config/

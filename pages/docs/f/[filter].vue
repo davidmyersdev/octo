55a73850858ad90b347ec140b455baaafe85c99f
David R. Myers <davidrmyersii@gmail.com>
2023-12-31 17:55:47 -0500

    Update dependencies

--- pages/docs/f/[filter].vue
+++ pages/docs/f/[filter].vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const props = defineProps<{ filter: string }>()
 const router = useRouter()
-const filterParam = computed(() => router.currentRoute.value.params.filter)
+const filterParam = computed(() => router.currentRoute.value.params.filter as string)
 const filter = computed(() => filterParam.value || props.filter || router.currentRoute.value.query.filter as string)
 const { query } = useRouteQuery()
 

1c2e52aee2fb12d835a3a4fd1fd5908d785f631a
David R. Myers <davidrmyersii@gmail.com>
2024-02-22 21:12:51 -0500

    Add Google sign-in healthcheck

--- test/e2e/healthchecks/index.spec.ts
+++ test/e2e/healthchecks/index.spec.ts
@@ -1,5 +1,36 @@
 import { expect, test } from '@playwright/test'
 
+test.describe('auth', () => {
+  test('google', async ({ page }) => {
+    await page.goto('https://accounts.google.com/')
+
+    const emailInput = page.locator('input[type="email"]')
+
+    await emailInput.fill(process.env.GOOGLE_TEST_USER_EMAIL!)
+    await emailInput.press('Enter')
+
+    const passwordInput = page.locator('input[type="password"]')
+
+    await passwordInput.fill(process.env.GOOGLE_TEST_USER_PASSWORD!)
+    await passwordInput.press('Enter')
+
+    await page.waitForLoadState('networkidle')
+    await page.goto('/')
+
+    const signInLink = page.getByText('Sign In')
+
+    await signInLink.click()
+
+    const googleLink = page.getByText('Continue with Google')
+
+    await googleLink.click()
+
+    await page.waitForLoadState('networkidle')
+
+    expect(page.url()).toMatch(/\/docs\/new$/)
+  })
+})
+
 test('sets the max-width of the editor to the readability max-width setting', async ({ page }) => {
   await page.goto('/docs/new')
 

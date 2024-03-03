042f400e99a74cdfff75fcfe4deaf91b272089d3
David R. Myers <davidrmyersii@gmail.com>
2024-03-03 11:55:25 -0500

    Add labels and togglability to `CoreNavPanel`

--- components/CoreNavPanel.vue
+++ components/CoreNavPanel.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
-import { type Component, computed } from 'vue'
+import { ChevronUpDownIcon } from '@heroicons/vue/24/outline'
+import { type Component, computed, ref } from 'vue'
 
 const props = defineProps<{
   as?: string | Component,
+  label?: string,
 }>()
 
 const component = computed(() => props.as || 'section')
+const showContent = ref(true)
+
+const toggleContent = () => {
+  showContent.value = !showContent.value
+}
 </script>
 
 <template>
@@ -13,6 +20,16 @@ const component = computed(() => props.as || 'section')
     :is="component"
     class="p-2"
   >
-    <slot />
+    <slot name="label">
+      <h6 v-if="label" class="flex items-center justify-between text-sm text-layer-muted cursor-pointer" @click="toggleContent">
+        <span class="flex items-center gap-2">
+          <ChevronUpDownIcon class="h-4" />
+          <span>{{ label }}</span>
+        </span>
+      </h6>
+    </slot>
+    <template v-if="showContent">
+      <slot />
+    </template>
   </component>
 </template>

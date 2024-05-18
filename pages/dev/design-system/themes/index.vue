0e49e92d36566d3ec3f7b349faeca1f46c15cfab
David R. Myers <davidrmyersii@gmail.com>
2024-05-18 16:20:09 -0400

    Start a design-system page to work on components

--- pages/dev/design-system/themes/index.vue
+++ pages/dev/design-system/themes/index.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
+import { DesignSystemColors } from '~/lib/components'
 </script>
 
 <template>
   <div class="flex flex-col gap-4 p-4">
-    <CoreSwitch />
+    <DesignSystemColors />
   </div>
 </template>

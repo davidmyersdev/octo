9feffc2a85ec16a47588211403b86a1a8d890125
David R. Myers <davidrmyersii@gmail.com>
2024-07-31 20:36:42 -0400

    Refresh the main menu

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,21 +1,24 @@
-<script lang="ts">
+<script lang="ts" setup>
 import {
-  UserCircleIcon as AccountIcon,
-  BeakerIcon,
-  CheckIcon,
-  DocumentIcon,
   DocumentTextIcon,
-  InboxIcon,
   CloudArrowUpIcon as SaveIcon,
-  MagnifyingGlassIcon as SearchIcon,
-  Cog8ToothIcon as SettingsIcon,
   HashtagIcon as TagIcon,
-  TrashIcon,
   Square2StackIcon as WorkspaceIcon,
-  Squares2X2Icon as WorkspacesIcon,
 } from '@heroicons/vue/24/outline'
+import {
+  IconCheck,
+  IconFile,
+  IconInbox,
+  IconLayoutGrid,
+  IconMessages,
+  IconNotebook,
+  IconSearch,
+  IconSettings,
+  IconTestPipe,
+  IconTrash,
+  IconUserCircle,
+} from '@tabler/icons-vue'
 import { nanoid } from 'nanoid'
-import CoreDivider from '#root/components/CoreDivider.vue'
 import CoreLink from '#root/components/CoreLink.vue'
 import ModKKey from '#root/components/ModKKey.vue'
 import TagLink from '#root/components/TagLink.vue'
@@ -23,237 +26,231 @@ import { DEACTIVATE_CONTEXT, SET_CONTEXT_TAGS } from '#root/src/store/actions'
 import { useFiles } from '#root/src/stores/useFiles'
 import { AsyncIterable } from '#root/src/utils/iterables'
 
-export default {
-  components: {
-    AccountIcon,
-    BeakerIcon,
-    CheckIcon,
-    CoreDivider,
-    CoreLink,
-    DocumentIcon,
-    DocumentTextIcon,
-    TagIcon,
-    InboxIcon,
-    ModKKey,
-    SaveIcon,
-    SearchIcon,
-    SettingsIcon,
-    TagLink,
-    TrashIcon,
-    WorkspaceIcon,
-    WorkspacesIcon,
-  },
-  setup() {
-    const { user } = useUser()
-    const { public: { fathomEventCtaSaveDocs, firebaseDisabled, linkFeedback } } = useConfig()
-    const mq = useMq()
-    const mediumPlus = computed(() => mq.value.mdPlus)
+const { user } = useUser()
+const { store } = useVuex()
+const { public: { fathomEventCtaSaveDocs, firebaseDisabled, linkFeedback } } = useConfig()
+const mq = useMq()
+const { pinnedDocs, unpinDoc } = usePinnedDocs()
+const { doc } = useDocs()
+const router = useRouter()
 
-    const trackCta = () => {
-      window.fathom?.trackGoal(fathomEventCtaSaveDocs, 0)
+const mediumPlus = computed(() => mq.value.mdPlus)
+const experimentalFeaturesEnabled = computed(() => store.state.settings.experimental)
+const context = computed(() => store.state.context)
+const contextTags = computed(() => [...context.value.tags].sort())
+const contexts = computed(() => store.getters.sortedContexts)
+const tags = computed(() => store.getters.tags)
+
+const clearContext = () => {
+  store.dispatch(DEACTIVATE_CONTEXT)
+}
+
+const openFile = async () => {
+  const id = nanoid()
+  // @ts-expect-error This API is valid but is not recognized by TypeScript.
+  const [handle] = await window.showOpenFilePicker({
+    types: [
+      {
+        description: 'Markdown Files',
+        accept: {
+          'text/markdown': ['.markdown', '.md'],
+        },
+      },
+    ],
+  })
+
+  try {
+    const { id } = await AsyncIterable(useFiles().files).any((file) => {
+      return file.handle.isSameEntry(handle)
+    })
+
+    if (id) {
+      return router.push({ path: `/file-editor/${id}` })
     }
+  } catch {}
 
-    return {
-      firebaseDisabled,
-      linkFeedback,
-      mediumPlus,
-      trackCta,
-      user,
-    }
-  },
-  computed: {
-    authIsEvaluated() {
-      return this.$store.state.auth.isEvaluated
-    },
-    context() {
-      return this.$store.state.context
-    },
-    contextTags() {
-      return [...this.context.tags].sort()
-    },
-    contexts() {
-      return this.$store.getters.sortedContexts
-    },
-    experimentalFeaturesEnabled() {
-      return this.$store.state.settings.experimental
-    },
-    tags() {
-      return this.$store.getters.tags
-    },
-  },
-  methods: {
-    clearContext() {
-      this.$store.dispatch(DEACTIVATE_CONTEXT)
-    },
-    async openFile() {
-      const id = nanoid()
-      // @ts-expect-error This API is valid but is not recognized by TypeScript.
-      const [handle] = await window.showOpenFilePicker({
-        types: [
-          {
-            description: 'Markdown Files',
-            accept: {
-              'text/markdown': ['.markdown', '.md'],
-            },
-          },
-        ],
-      })
+  await handle.createWritable()
 
-      try {
-        const { id } = await AsyncIterable(useFiles().files).any((file) => {
-          return file.handle.isSameEntry(handle)
-        })
+  useFiles().add({ id, handle })
 
-        if (id) {
-          return this.$router.push({ path: `/file-editor/${id}` })
-        }
-      } catch {}
+  return router.push({ path: `/file-editor/${id}` })
+}
 
-      await handle.createWritable()
+const setContext = (context: unknown) => {
+  store.dispatch(SET_CONTEXT_TAGS, { context })
+}
 
-      useFiles().add({ id, handle })
-      return this.$router.push({ path: `/file-editor/${id}` })
-    },
-    setContext(context: unknown) {
-      this.$store.dispatch(SET_CONTEXT_TAGS, { context })
-    },
-  },
+const closeTab = async (id: string) => {
+  if (doc.value?.id === id) {
+    await router.push({ path: '/docs/new' })
+  }
+
+  unpinDoc(id)
+}
+
+const trackCta = () => {
+  window.fathom?.trackGoal(fathomEventCtaSaveDocs, 0)
 }
 </script>
 
 <template>
-  <CoreScrollable>
-    <CoreNavPanel class="flex flex-col gap-1">
-      <CoreLink :to="{ path: '/docs' }" class="sidebar-link">
-        <div class="flex gap-3 items-center">
-          <SearchIcon class="sq-5" />
-          <span>Search</span>
+  <CoreScrollable class="p-1">
+    <div class="flex flex-col gap-1">
+      <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-1">
+          <CoreButton :as="CoreLink" :to="{ path: '/docs' }" class="sidebar-link">
+            <IconSearch :stroke-width="1.25" class="sq-5" />
+            <span class="grow">Search</span>
+          </CoreButton>
         </div>
-      </CoreLink>
-      <CoreLink :to="{ path: '/docs/f/untagged' }" class="sidebar-link">
-        <InboxIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>Inbox</span>
-          <ModKKey class="text-layer-muted">U</ModKKey>
-        </span>
-      </CoreLink>
-      <CoreLink :to="{ path: '/notepad' }" class="sidebar-link">
-        <div class="flex gap-3 items-center">
-          <CoreAsset type="Daily" class="sq-5" />
-          <span>Daily Notepad</span>
+        <CoreLayer class="flex flex-col gap-1">
+          <FlexDivider class="-mx-1" />
+          <DashPanel class="flex flex-col bg-layer" label="Recent">
+            <div v-if="pinnedDocs.length > 0" class="flex flex-col gap-1">
+              <CoreButton v-for="pinnedDoc in pinnedDocs" :key="pinnedDoc.id" :as="CoreLink" :to="{ path: `/docs/${pinnedDoc.id}` }" class="allow-link-active">
+                <span class="flex-grow text-ellipsis overflow-hidden">{{ pinnedDoc.label }}</span>
+                <CoreLayer class="flex -mr-1">
+                  <CoreButton class="p-1 bg-transparent" @click.prevent.stop="() => closeTab(pinnedDoc.id)">
+                    <Icon name="Dismiss" size="1rem" />
+                  </CoreButton>
+                </CoreLayer>
+              </CoreButton>
+            </div>
+            <div v-else>
+              <span class="text-layer-muted">Nothing to see here...</span>
+            </div>
+          </DashPanel>
+        </CoreLayer>
+        <FlexDivider class="-mx-1" />
+        <div class="flex flex-col gap-1">
+          <CoreLink :to="{ path: '/docs/f/untagged' }" class="sidebar-link">
+            <IconInbox :stroke-width="1.25" class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>Inbox</span>
+              <ModKKey class="text-layer-muted">U</ModKKey>
+            </span>
+          </CoreLink>
+          <CoreLink :to="{ path: '/notepad' }" class="sidebar-link">
+            <div class="flex gap-2 items-center">
+              <IconNotebook :stroke-width="1.25" class="sq-5" />
+              <span>Daily Notepad</span>
+            </div>
+          </CoreLink>
+          <CoreLink :to="{ path: '/workspaces' }" class="sidebar-link">
+            <IconLayoutGrid :stroke-width="1.25" class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span class="action">Workspaces</span>
+              <ModKKey class="text-layer-muted">C</ModKKey>
+            </span>
+          </CoreLink>
         </div>
-      </CoreLink>
-      <CoreLink :to="{ path: '/workspaces' }" class="sidebar-link">
-        <WorkspacesIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span class="action">Workspaces</span>
-          <ModKKey class="text-layer-muted">C</ModKKey>
-        </span>
-      </CoreLink>
-      <CoreLink :to="linkFeedback" class="sidebar-link">
-        <div class="flex gap-3 items-center">
-          <CoreAsset type="Feedback" class="sq-5" />
-          <span>Feedback</span>
+        <div v-if="experimentalFeaturesEnabled" class="flex flex-col gap-1">
+          <FlexDivider class="-mx-1" />
+          <button class="sidebar-link w-full" @click="openFile">
+            <IconFile :stroke-width="1.25" class="sq-5" />
+            <span class="action flex flex-grow items-center justify-between ml-2">
+              <span>Open File</span>
+              <IconTestPipe :stroke-width="1.25" class="text-layer-muted w-5" title="Experimental" />
+            </span>
+          </button>
+          <CoreLink :to="{ path: '/assistant' }" class="sidebar-link allow-link-active">
+            <CoreAsset type="Logo" class="sq-5" />
+            <span class="action flex flex-grow items-center justify-between ml-2">
+              <span>Assistant</span>
+              <IconTestPipe :stroke-width="1.25" class="text-layer-muted w-5" title="Experimental" />
+            </span>
+          </CoreLink>
         </div>
-      </CoreLink>
-      <div v-if="experimentalFeaturesEnabled">
-        <button class="sidebar-link w-full" title="Experimental" @click="openFile">
-          <DocumentIcon class="sq-5" />
-          <span class="action flex flex-grow items-center justify-between ml-3">
-            <span>Open File</span>
-            <BeakerIcon class="text-layer-muted w-5" />
-          </span>
-        </button>
+        <FlexDivider class="-mx-1" />
+        <div class="flex flex-col gap-1">
+          <CoreLink :to="linkFeedback" class="sidebar-link">
+            <div class="flex gap-3 items-center">
+              <IconMessages :stroke-width="1.25" class="sq-5" />
+              <span>Feedback</span>
+            </div>
+          </CoreLink>
+          <CoreLink v-if="!firebaseDisabled && !user.id" :to="{ path: '/account' }" class="sidebar-link text-brand" @click="trackCta">
+            <SaveIcon class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>Save Docs</span>
+            </span>
+          </CoreLink>
+          <CoreLink :to="{ path: '/settings' }" class="sidebar-link">
+            <div class="flex gap-3 items-center">
+              <IconSettings :stroke-width="1.25" class="sq-5" />
+              <span>Settings</span>
+            </div>
+          </CoreLink>
+          <CoreLink :to="{ path: '/account' }" class="sidebar-link">
+            <div class="flex gap-3 items-center">
+              <IconUserCircle :stroke-width="1.25" class="sq-5" />
+              <span>Account</span>
+            </div>
+          </CoreLink>
+        </div>
       </div>
-      <CoreLink v-if="experimentalFeaturesEnabled" :to="{ path: '/assistant' }" class="sidebar-link allow-link-active">
-        <CoreAsset type="Logo" class="sq-5" />
-        <span class="action flex flex-grow items-center justify-between ml-3">
-          <span>Assistant</span>
-          <BeakerIcon class="text-layer-muted w-5" />
-        </span>
-      </CoreLink>
-      <CoreLink v-if="!firebaseDisabled && !user.id" :to="{ path: '/account' }" class="sidebar-link text-brand" @click="trackCta">
-        <SaveIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>Save Docs</span>
-        </span>
-      </CoreLink>
-      <CoreLink :to="{ path: '/settings' }" class="sidebar-link">
-        <div class="flex gap-3 items-center">
-          <SettingsIcon class="sq-5" />
-          <span>Settings</span>
-        </div>
-      </CoreLink>
-      <CoreLink :to="{ path: '/account' }" class="sidebar-link">
-        <div class="flex gap-3 items-center">
-          <AccountIcon class="sq-5" />
-          <span>Account</span>
-        </div>
-      </CoreLink>
-    </CoreNavPanel>
-    <CoreDivider />
-    <CoreNavPanel class="flex flex-col gap-1" label="Filters">
-      <CoreLink :to="{ path: '/docs/f/tasks' }" class="sidebar-link">
-        <CheckIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>Tasks</span>
-          <ModKKey class="text-layer-muted">T</ModKKey>
-        </span>
-      </CoreLink>
-      <CoreLink :to="{ path: '/docs/f/discarded' }" class="sidebar-link">
-        <TrashIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>Discarded</span>
-          <ModKKey class="text-layer-muted">D</ModKKey>
-        </span>
-      </CoreLink>
-    </CoreNavPanel>
-    <CoreDivider v-if="context.active || contextTags.length" />
-    <CoreNavPanel v-if="context.active || contextTags.length" label="Workspace">
-      <template #label-alt-action>
-        <CoreButton class="p-0.5" @click.stop="clearContext">
-          <svg class="sq-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </CoreButton>
-      </template>
-      <div class="flex flex-col gap-2 md:gap-1">
-        <TagLink v-for="tag in contextTags" :key="tag" :tag="tag" class="sidebar-link" />
-      </div>
-    </CoreNavPanel>
-    <CoreDivider v-if="contexts.length" />
-    <CoreNavPanel v-if="contexts.length" class="flex flex-col gap-1" label="Workspaces">
-      <button v-for="workspace in contexts" :key="workspace.id" class="sidebar-link w-full" @click="setContext(workspace)">
-        <WorkspaceIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>{{ workspace.name }}</span>
-        </span>
-      </button>
-    </CoreNavPanel>
-    <CoreDivider v-if="tags.length" />
-    <CoreNavPanel v-if="tags.length" class="flex flex-col gap-1" label="Tags">
-      <CoreLink class="sidebar-link md:hidden" :to="{ path: '/tags' }">
-        <TagIcon class="sq-5" />
-        <span class="ml-3 flex-grow">Tags</span>
-      </CoreLink>
-      <div v-if="mediumPlus" class="flex flex-col gap-1">
-        <TagLink v-for="tag in tags" :key="tag" :tag="tag" class="sidebar-link" />
-      </div>
-    </CoreNavPanel>
-    <CoreDivider />
-    <CoreNavPanel class="flex flex-col gap-1" label="Policies">
-      <CoreLink :to="{ path: '/privacy-policy' }" class="sidebar-link">
-        <DocumentTextIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>Privacy Policy</span>
-        </span>
-      </CoreLink>
-      <CoreLink :to="{ path: '/terms-and-conditions' }" class="sidebar-link">
-        <DocumentTextIcon class="sq-5" />
-        <span class="action flex flex-grow items-stretch justify-between ml-3">
-          <span>Terms &amp; Conditions</span>
-        </span>
-      </CoreLink>
-    </CoreNavPanel>
+      <FlexDivider class="-mx-1" />
+      <CoreLayer class="flex flex-col gap-1">
+        <DashPanel class="flex flex-col bg-layer" collapsed label="Filters">
+          <CoreLink :to="{ path: '/docs/f/tasks' }" class="sidebar-link">
+            <IconCheck :stroke-width="1.25" class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>Tasks</span>
+              <ModKKey class="text-layer-muted">T</ModKKey>
+            </span>
+          </CoreLink>
+          <CoreLink :to="{ path: '/docs/f/discarded' }" class="sidebar-link">
+            <IconTrash :stroke-width="1.25" class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>Discarded</span>
+              <ModKKey class="text-layer-muted">D</ModKKey>
+            </span>
+          </CoreLink>
+        </DashPanel>
+        <DashPanel v-if="context.active || contextTags.length" class="bg-layer" label="Workspace">
+          <template #label-alt-action>
+            <CoreButton class="p-0.5" @click.stop="clearContext">
+              <svg class="sq-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </CoreButton>
+          </template>
+          <div class="flex flex-col gap-2 md:gap-1">
+            <TagLink v-for="tag in contextTags" :key="tag" :tag="tag" class="sidebar-link" />
+          </div>
+        </DashPanel>
+        <DashPanel v-if="contexts.length" class="flex flex-col bg-layer" collapsed label="Workspaces">
+          <button v-for="workspace in contexts" :key="workspace.id" class="sidebar-link w-full" @click="setContext(workspace)">
+            <WorkspaceIcon class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>{{ workspace.name }}</span>
+            </span>
+          </button>
+        </DashPanel>
+        <DashPanel v-if="tags.length" class="flex flex-col bg-layer" collapsed label="Tags">
+          <CoreLink class="sidebar-link md:hidden" :to="{ path: '/tags' }">
+            <TagIcon class="sq-5" />
+            <span class="ml-3 flex-grow">Tags</span>
+          </CoreLink>
+          <div v-if="mediumPlus" class="flex flex-col gap-1">
+            <TagLink v-for="tag in tags" :key="tag" :tag="tag" class="sidebar-link" />
+          </div>
+        </DashPanel>
+        <DashPanel class="flex flex-col bg-layer" collapsed label="Policies">
+          <CoreLink :to="{ path: '/privacy-policy' }" class="sidebar-link">
+            <DocumentTextIcon class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>Privacy Policy</span>
+            </span>
+          </CoreLink>
+          <CoreLink :to="{ path: '/terms-and-conditions' }" class="sidebar-link">
+            <DocumentTextIcon class="sq-5" />
+            <span class="action flex flex-grow items-stretch justify-between ml-2">
+              <span>Terms &amp; Conditions</span>
+            </span>
+          </CoreLink>
+        </DashPanel>
+      </CoreLayer>
+    </div>
   </CoreScrollable>
 </template>

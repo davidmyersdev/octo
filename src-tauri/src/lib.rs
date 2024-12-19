3af7fa0a3ef05d48627434438dee4e2b7795c810
David R. Myers <davidrmyersii@gmail.com>
2024-12-19 15:39:49 -0500

    Add Tauri updater plugin
    https://tauri.app/plugin/updater/

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_updater::Builder::new().build())
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(

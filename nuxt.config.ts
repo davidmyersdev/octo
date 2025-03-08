68ee7387f3f9a62ac75f59cb73e7a77d2b50f86f
David R. Myers <davidrmyersii@gmail.com>
2025-03-08 15:46:40 -0500

    Ignore `/_routes` during pre-render (fixes windows build error)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -130,6 +130,9 @@ export default defineNuxtConfig({
         '/product/pricing',
         '/settings',
       ],
+      ignore: [
+        '/_routes',
+      ],
     },
     routeRules: {
       '/emulator/auth/**': {
